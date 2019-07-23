cad7026c058f88ed2947b7222005cd9a7d821a9b
vijay <krishna.vijayb8@gmail.com>
2019-07-23 07:14:58 +0200

    refine middleware

--- pkg/platform/http/middleware.go
+++ pkg/platform/http/middleware.go
@@ -10,9 +10,8 @@ import (
 func CORSMiddleware() func(http.Handler) http.Handler {
 	return cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
+		AllowedMethods:   []string{"GET"},
+		AllowedHeaders:   []string{"Accept", "Content-Type"},
 		AllowCredentials: true,
 		MaxAge:           300,
 	}).Handler
